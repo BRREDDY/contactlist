3c8f2e73514bd9e0f3c5d5e8d2c9e7e27beda52a
BRREDDY <BRREDDY@users.noreply.github.com>
2017-06-03 09:42:24 -0500

    2nd commit

--- contactlist/routes/route.js
+++ contactlist/routes/route.js
@@ -1,23 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const Contact = require('../models/contacts');
 
 //retrieving contact
 router.get('/contacts', (req,res,next)=>{
-    res.send('Retrieving the contact list');
+    Contact.find(function(err,contacts)
+    {
+        res.json(contacts);
+
+    });
 });
 
 //add contact 
 router.post('/contact',(req,res,next)=>{
 
-    //logic to add contact
+    let newContact = new Contact({
+        fist_name:req.body.fist_name,
+        last_name:req.body.last_name,
+        phone:req.body.phone
+});
+
+newContact.save((err,contact)=>{
+    if(err)
+    {
+        res.json({msg: 'Failed to add contact'});
+    }
+    else
+    {
+        res.json({msg: 'Contact added successfully'});
+    }
+
+    });
 
 });
+        
+  
 
 //delete contact 
 router.delete('/contact/:id',(req,res,next)=>{
-
-    //logic to add contact
-    
+    Contact.remove({_id: req.params.id}, function(err,result){
+        if(err)
+        {
+            res.json(err);
+        }
+        else
+        {
+            res.json(result);
+        }
+    });
+        
 });
 
 
